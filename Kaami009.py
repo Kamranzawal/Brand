86bcfe306d7d58f6ca8ab91263dc93408c844418
Kamranzawal <105263028+Kamranzawal@users.noreply.github.com>
2022-06-10 18:23:04 +0500

    Update Kaami009.py

--- Kaami009.py
+++ Kaami009.py
@@ -1,14 +1,9 @@
 import os
-
 os.system(' git pull ')
-
 os.system('termux-setup-storage')
-
-print("\033[:32m     Welcome To The My Tool")
-
+print("\033[:32m     Welcome To The My Tool")
 import Kaami09
 
-main() 
 
 
 
